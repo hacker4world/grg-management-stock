7e4de30301659565c8843412c7847e6f51e7d78f
hacker4world <zonegaming514@gmail.com>
2026-03-01 15:05:11 +0100

    .....

--- stock-backend/src/data-source.ts
+++ stock-backend/src/data-source.ts
@@ -27,7 +27,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
-  password: "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
